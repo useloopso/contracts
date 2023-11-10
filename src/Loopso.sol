4b399cd059fcf1f433c238a78f8400e99ff84a32
lajosdeme <lajosd@protonmail.ch>
2023-11-10 17:41:47 +0100

    complete first implementation of bridge, add comments

--- src/Loopso.sol
+++ src/Loopso.sol
@@ -10,10 +10,9 @@ import "./interfaces/ITokenFactory.sol";
 import "./interfaces/ILSP7Bridged.sol";
 import "./interfaces/ILSP8Bridged.sol";
 
+// TODO implement fee mechanism
 contract Loopso is AccessControl, ILoopso, IERC721Receiver {
     bytes32 public constant RELAYER_ROLE = keccak256("RELAYER_ROLE");
-
-    // TODO: uint256 public BRIDGE_FEE
 
     mapping(bytes32 => TokenAttestation) public attestedTokens; // from token ID to TokenAttestation on dest chain
     mapping(bytes32  => TokenTransfer) public tokenTransfers; // from transfer ID to transfer on source chain
@@ -36,6 +35,10 @@ contract Loopso is AccessControl, ILoopso, IERC721Receiver {
         _;
     }
 
+    /* ============================================== */
+    /*  =============  ADD NEW TOKEN  ==============  */
+    /* ============================================== */
+    /** @dev See ILoopso.sol - attestToken */
     function attestToken(TokenAttestation memory attestation) external onlyAdmin {
         address _newTokenAddress;
 
@@ -51,17 +54,19 @@ contract Loopso is AccessControl, ILoopso, IERC721Receiver {
         emit TokenAttested(attestationID);
     }
 
+    /* ============================================== */
+    /*  ==============  BRIDGE ERC20  ==============  */
+    /* ============================================== */
+    /** @dev See ILoopso.sol - bridgeTokens */
     function bridgeTokens(
         address _token,
         uint256 _amount,
         uint256 _dstChain,
         address _dstAddress
     ) external {
-        // 0. TODO - calculate fee paid to bridge & add fee to _amount
-        // 1. transfer _amount tokens to the bridge from msg.sender
         bool success = IERC20(_token).transferFrom(msg.sender, address(this), _amount);
         require(success, "Transfer failed. Make sure bridge is approved to spend tokens.");
-        // 2. create tokenTransfer struct
+      
         TokenTransfer memory _transfer = TokenTransfer(
             TokenTransferBase(
                 block.timestamp,
@@ -74,14 +79,44 @@ contract Loopso is AccessControl, ILoopso, IERC721Receiver {
             _amount
         );
         bytes32 _transferID = keccak256(abi.encodePacked(block.timestamp, block.chainid, msg.sender,_dstChain,_dstAddress,_token,_amount));
-        // 3. save tokenTransfer struct
+        
         tokenTransfers[_transferID] = _transfer;
-        // 4. emit event
+   
         emit TokensBridged(_transferID, TokenType.Fungible);
     }
 
+    /** @dev See ILoopso.sol - bridgeTokensBack */
+    function bridgeTokensBack(uint256 _amount, address _to, bytes32 _attestationID) external {
+        TokenAttestation memory attestedToken = attestedTokens[_attestationID];
+        require(attestedToken.tokenAddress != address(0), "Invalid _attestationID");
+      
+        ILSP7Bridged(attestedToken.wrappedTokenAddress).burn(msg.sender, _amount, new bytes(0));
+      
+        emit TokensBridgedBack(_amount, _to, _attestationID);
+    }
+
+    /** @dev See ILoopso.sol - releaseTokens */
+    function releaseTokens(uint256 _amount, address _to, address _token) external onlyRelayer {
+        bool success = IERC20(_token).transfer(_to, _amount);
+        require(success, "Failed to payout tokens");
+    }
+
+    /** @dev See ILoopso.sol - releaseWrappedTokens */
+    function releaseWrappedTokens(uint256 _amount, address _to, bytes32 _attestationID) external onlyRelayer {
+        // 1. get the attested token
+        TokenAttestation memory attestedToken = attestedTokens[_attestationID];
+        require(attestedToken.tokenAddress != address(0), "Invalid _attestationID");
+        // 2. mint amount of tokens to the _to address
+        ILSP7Bridged(attestedToken.wrappedTokenAddress).mint(_to, _amount, true, new bytes(0));
+        // 3. emit event
+        emit WrappedTokensReleased(_amount, _to, _attestationID);
+    }
+
+    /* ============================================== */
+    /*  =============  BRIDGE ERC721  ==============  */
+    /* ============================================== */
+    /** @dev See ILoopso.sol - bridgeNonFungibleTokens */
     function bridgeNonFungibleTokens(address _token, uint256 _tokenID, string memory tokenURI, uint256 _dstChain, address _dstAddress) external {
-        // 0. TODO: take fixed fee on NFT brdiging
         // transfer IERC721 from user to bridge
         IERC721(_token).safeTransferFrom(msg.sender, address(this), _tokenID);
         // create token transfer struct
@@ -104,16 +139,7 @@ contract Loopso is AccessControl, ILoopso, IERC721Receiver {
         emit TokensBridged(_transferID, TokenType.NonFungible);
     }
 
-    function bridgeTokensBack(uint256 _amount, address _to, bytes32 _attestationID) external {
-        // 1. get attested token
-        TokenAttestation memory attestedToken = attestedTokens[_attestationID];
-        require(attestedToken.tokenAddress != address(0), "Invalid _attestationID");
-        // 2. burn amount of tokens
-        ILSP7Bridged(attestedToken.wrappedTokenAddress).burn(msg.sender, _amount, new bytes(0));
-        // 3. emit proper event
-        emit TokensBridgedBack(_amount, _to, _attestationID);
-    }
-
+    /** @dev See ILoopso.sol - bridgeNonFungibleTokensBack */
     function bridgeNonFungibleTokensBack(uint256 _tokenId, address _to, bytes32 _attestationID) external {
         TokenAttestation memory attestedToken = attestedTokens[_attestationID];
         require(attestedToken.tokenAddress != address(0), "Invalid _attestationID");
@@ -121,16 +147,7 @@ contract Loopso is AccessControl, ILoopso, IERC721Receiver {
         emit NonFungibleTokensBridgedBack(_tokenId, _to, _attestationID);
     }
 
-    function releaseWrappedTokens(uint256 _amount, address _to, bytes32 _attestationID) external onlyRelayer {
-        // 1. get the attested token
-        TokenAttestation memory attestedToken = attestedTokens[_attestationID];
-        require(attestedToken.tokenAddress != address(0), "Invalid _attestationID");
-        // 2. mint amount of tokens to the _to address
-        ILSP7Bridged(attestedToken.wrappedTokenAddress).mint(_to, _amount, false, new bytes(0));
-        // 3. emit event
-        emit WrappedTokensReleased(_amount, _to, _attestationID);
-    }
-
+    /** @dev See ILoopso.sol - releaseWrappedNonFungibleTokens */
     function releaseWrappedNonFungibleTokens(uint256 _tokenId, string calldata _tokenURI, address _to, bytes32 _attestationID) external {
         // 1. get the attested token
         TokenAttestation memory attestedToken = attestedTokens[_attestationID];
@@ -141,20 +158,22 @@ contract Loopso is AccessControl, ILoopso, IERC721Receiver {
         emit WrappedNonFungibleTokensReleased(_tokenId, _to, _attestationID);
     }
 
-    function releaseTokens(uint256 _amount, address _to, address _token) external onlyRelayer {
-        bool success = IERC20(_token).transfer(_to, _amount);
-        require(success, "Failed to payout tokens");
-    }
-
+    /** @dev See ILoopso.sol - releaseNonFungibleTokens */
     function releaseNonFungibleTokens(uint256 _tokenId, address _to, address _token) external {
         IERC721(_token).safeTransferFrom(address(this), _to, _tokenId);
     }
 
+    /* ============================================== */
+    /*  =================  ADMIN  ==================  */
+    /* ============================================== */
     function setTokenFactory(ITokenFactory _tokenFactory) external onlyAdmin {
         require(address(_tokenFactory) != address(0), "Can't set LSP Factory to the zero address");
         tokenFactory = _tokenFactory;
     }
-
+    
+    /* ============================================== */
+    /*  ============= ERC721 RECEIVER ==============  */
+    /* ============================================== */
     function onERC721Received(address /* operator */, address /* from */, uint256 /* tokenId */, bytes calldata /* data */) external pure returns (bytes4) {
         return IERC721Receiver.onERC721Received.selector;
     }
--- src/interfaces/ILoopso.sol
+++ src/interfaces/ILoopso.sol
@@ -7,6 +7,20 @@ interface ILoopso {
         NonFungible
     }
 
+    /* ============================================== */
+    /*  =================  STRUCTS  ================  */
+    /* ============================================== */
+
+    /**
+    @dev Stores info about a chain A token on chain B.
+    @param tokenAddress Address of token on chain A
+    @param tokenChain chain ID of chain A
+    @param tokenType Whether the token is fungible/non-fungible
+    @param decimals Only applicable to fungible tokens. Number of decimals.
+    @param symbol Token symbol
+    @param name Token name
+    @param wrappedTokenAddress The address of the token after it has been deployed on chain B
+     */
     struct TokenAttestation {
         address tokenAddress;
         uint256 tokenChain;
@@ -17,6 +31,15 @@ interface ILoopso {
         address wrappedTokenAddress;
     }
 
+    /**
+    @dev Stores info about a token transfer from chain A to chain B
+    @param timestamp When the transfer happened
+    @param srcChain The chain ID where the transfer was initiated
+    @param srcAddress The address of the account that initiated the transfer
+    @param dstChain Chain ID where the transfer is being sent to
+    @param dstAddress Address on chain B that will receive the transfer
+    @param tokenAddress Address of token on chain A that is being transferred  
+     */
     struct TokenTransferBase {
         uint256 timestamp;
         uint256 srcChain;
@@ -26,36 +49,79 @@ interface ILoopso {
         address tokenAddress;
     }
 
+    /**
+    @dev Used for fungible token transfers
+    @param tokenTransfer Base token transfer info struct
+    @param amount The amount of tokens being transferred
+     */
     struct TokenTransfer {
         TokenTransferBase tokenTransfer;
         uint256 amount;
     }
 
+    /**
+    @dev Used for non-fungible token transfers
+    @param tokenTransfer Base token transfer info struct
+    @param tokenID ID of the NFT being transferred
+    @param tokenURI Token URI of the NFT being transferred
+     */
     struct TokenTransferNonFungible {
         TokenTransferBase tokenTransfer;
         uint256 tokenID;
         string tokenURI;
     }
 
+    /* ============================================== */
+    /*  =================  EVENTS  =================  */
+    /* ============================================== */
+
     /** @dev Emitted by attestToken. Means a new token is supported by the bridge. */
     event TokenAttested(bytes32 indexed attestationID);
 
-    /** @dev Emitted by bridgeTokens. transferID is used by the relayer as it helps locate the transfer details. */
-    event TokensBridged(bytes32 indexed transferID, TokenType indexed tokenType);
+    /** @dev Emitted by bridgeTokens. transferID is used by the relayer to locate the transfer details. */
+    event TokensBridged(
+        bytes32 indexed transferID,
+        TokenType indexed tokenType
+    );
 
     /** @dev Emitted by bridgeTokensBack. Relayer should use indexed params to release the tokens on the other chain. */
-    event TokensBridgedBack(uint256 indexed amount, address indexed to, bytes32 indexed attestationID);
+    event TokensBridgedBack(
+        uint256 indexed amount,
+        address indexed to,
+        bytes32 indexed attestationID
+    );
 
-    event NonFungibleTokensBridgedBack(uint256 indexed tokenId, address indexed to, bytes32 indexed attestationID);
+    /** @dev Emitted by bridgeNonFungibleTokens. Relayer should use indexed params to release the tokens on the other chain. */
+    event NonFungibleTokensBridgedBack(
+        uint256 indexed tokenId,
+        address indexed to,
+        bytes32 indexed attestationID
+    );
 
     /** @dev Emitted by releaseTokens. This means briding back wrapped tokens to the source chain was successful. */
-    event TokensReleased(uint256 indexed amount, address indexed to, address indexed token);
+    event TokensReleased(
+        uint256 indexed amount,
+        address indexed to,
+        address indexed token
+    );
 
-    /** @dev Emitted by releaseWrappedTokens. This briding tokens was succesful. */
-    event WrappedTokensReleased(uint256 indexed amount, address indexed to, bytes32 indexed attestationID);
-    
-    event WrappedNonFungibleTokensReleased(uint256 indexed tokenId, address indexed to, bytes32 indexed attestationID);
+    /** @dev Emitted by releaseWrappedTokens. This means briding tokens was successful. */
+    event WrappedTokensReleased(
+        uint256 indexed amount,
+        address indexed to,
+        bytes32 indexed attestationID
+    );
 
+    /** @dev Emitted by releaseWrappedNonFungibleTokens. This means bridging tokens was successful. */
+    event WrappedNonFungibleTokensReleased(
+        uint256 indexed tokenId,
+        address indexed to,
+        bytes32 indexed attestationID
+    );
+
+    /* ============================================== */
+    /*  =============  ADD NEW TOKEN  ==============  */
+    /* ============================================== */
     /**
     @dev Called by bridge admin on the destination chain to attest a token.
     Only attested tokens can be bridged.
@@ -63,8 +129,12 @@ interface ILoopso {
      */
     function attestToken(TokenAttestation memory attestation) external;
 
+    /* ============================================== */
+    /*  ==============  BRIDGE ERC20  ==============  */
+    /* ============================================== */
+
     /**
-    @dev Called by users to bridge tokens from source chain to destination chain.
+    @dev Called by users to bridge ERC20 tokens from source chain to destination chain.
     @param _token The address of the token to bridge.
     @param _amount The amount of tokens to bridge.
     @param _dstChain Chain ID where we are bridging to.
@@ -78,21 +148,29 @@ interface ILoopso {
     ) external;
 
     /**
-    @dev Burns _amount of locked tokens on the destination chain, emits an event that relayer picks up.
+    @dev Burns _amount of locked ERC20 tokens on the destination chain, emits an event that relayer picks up.
     @param _amount Amount of tokens to bridge back.
     @param _to Address the original tokens will be released to.
     @param _attestationID ID of the TokenAttestation struct we use to identify a chain A token on chain B.
      */
-    function bridgeTokensBack(uint256 _amount, address _to, bytes32 _attestationID) external;
+    function bridgeTokensBack(
+        uint256 _amount,
+        address _to,
+        bytes32 _attestationID
+    ) external;
 
     /**
-    @dev Called by the relayer after it picks up a TokensBridgedBackEvent.
+    @dev Called by the relayer after it picks up a TokensBridgedBack event.
     It releases the bridged back tokens to the user.
     @param _amount The amount of tokens to release.
     @param _to The address to release the tokens to.
     @param _token The address of the token to release.
      */
-    function releaseTokens(uint256 _amount, address _to, address _token) external;
+    function releaseTokens(
+        uint256 _amount,
+        address _to,
+        address _token
+    ) external;
 
     /**
     @dev Called by the relayer after it picks up a bridge event from the source chain.
@@ -101,5 +179,50 @@ interface ILoopso {
     @param _to The address the wrapped tokens will be minted to.
     @param _attestationID The ID we use to identify a chain A token on chain B.
      */
-    function releaseWrappedTokens(uint256 _amount, address _to, bytes32 _attestationID) external;
+    function releaseWrappedTokens(
+        uint256 _amount,
+        address _to,
+        bytes32 _attestationID
+    ) external;
+
+    /* ============================================== */
+    /*  =============  BRIDGE ERC721  ==============  */
+    /* ============================================== */
+
+    /**
+    @dev Called by users to bridge ERC721 tokens from source chain to destination chain.
+    @param _token The address of the ERC721 to bridge
+    @param _tokenID The ID of the token to bridge
+    @param tokenURI Token URI of the ERC721
+    @param _dstChain Destination chain ID
+    @param _dstAddress Address that will receive the ERC721 on the destination chain     
+     */
+    function bridgeNonFungibleTokens(address _token, uint256 _tokenID, string memory tokenURI, uint256 _dstChain, address _dstAddress) external;
+
+    /**
+    @dev Burns the wrapped ERC721 with token ID on the destination chain, and emits an event that the relayer picks up.
+    @param _tokenId The token ID of the ERC721
+    @param _to The address that will receive the original ERC721 on the source chain
+    @param _attestationID ID of the TokenAttestation struct we use to identify a chain A token on chain B.
+     */
+    function bridgeNonFungibleTokensBack(uint256 _tokenId, address _to, bytes32 _attestationID) external;
+
+    /**
+    @dev Called by the relayer after it picks up a bridge event from the source chain.
+    It mints a wrapped ERC721 with the given token ID, setting the token URI as well.
+    @param _tokenId The token ID of the NFT
+    @param _tokenURI The token URI
+    @param _to The address that will receive the NFT on the destination chain
+    @param _attestationID ID of the TokenAttestation struct we use to identify a chain A token on chain B.
+     */
+    function releaseWrappedNonFungibleTokens(uint256 _tokenId, string calldata _tokenURI, address _to, bytes32 _attestationID) external;
+
+    /**
+    @dev Called by the relayer after it picks up a NonFungibleTokensBridgedBack event.
+    It releases the bridged back token to the user.
+    @param _tokenId The token ID of the NFT
+    @param _to The address that will receive the NFT on the destination chain
+    @param _token The address of the NFT on the source chain
+     */
+    function releaseNonFungibleTokens(uint256 _tokenId, address _to, address _token) external;    
 }
